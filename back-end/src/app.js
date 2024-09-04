2e2aa33ee3bc68ccc5e739bb7824c0df622ea2aa
Yago Mouro <yagormouro@gmail.com>
2024-09-03 23:04:50 -0300

    feat: first commit

---
+++ back-end/src/app.js
@@ -0,0 +1,18 @@
+import express, { json, urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+export default app
---
+++ back-end/src/controllers/categorias.js
@@ -0,0 +1,13 @@
+import { prisma } from '../database/client.js'
+
+const controller = {}
+
+controller.create = async function(req, res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export default controller
