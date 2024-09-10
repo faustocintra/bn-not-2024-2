5224681fa03ab41c207243ed97060a9ca1fab5f8
ThiagoResende88 <thiagod.resende15@gmail.com>
2024-09-10 16:04:19 -0300

    (09/09) Término dos CRUDs simples

---
+++ back-end/src/app.js
@@ -0,0 +1,27 @@
+import express, { json, urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+import categoriasRouter from './routes/categorias.js'
+app.use('/categorias', categoriasRouter)
+
+import clientesRouter from './routes/clientes.js'
+app.use('/clientes', clientesRouter)
+
+import fornecedoresRouter from './routes/fornecedores.js'
+app.use('/fornecedores', fornecedoresRouter)
+
+export default app
