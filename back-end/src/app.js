63b7ff031b0ff7b034d0362acd5f84a98981dbf6
Fausto G. Cintra <professor@faustocintra.com.br>
2024-09-09 22:02:55 -0300

    (09/09) Término dos CRUDs simples

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -20,4 +20,10 @@ app.use('/users', usersRouter)
 import categoriasRouter from './routes/categorias.js'
 app.use('/categorias', categoriasRouter)
 
+import clientesRouter from './routes/clientes.js'
+app.use('/clientes', clientesRouter)
+
+import fornecedoresRouter from './routes/fornecedores.js'
+app.use('/fornecedores', fornecedoresRouter)
+
 export default app
