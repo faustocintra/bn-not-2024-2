373fd1b71b69e1a27f7ace95b22f7ac130158040
Nathan <nathan.oliveira@cloudwalk.io>
2024-09-23 20:23:50 -0300

    (23/09) Inicio da Logica CRUD do item de venda

--- back-end/src/controllers/fornecedores.js
+++ back-end/src/controllers/fornecedores.js
@@ -15,7 +15,7 @@ controller.create = async function(req, res) {
     // HTTP 201: Created
     res.status(201).end()
   }
-  catch(error) {
+  catch (error) {
     // Deu errado: exibe o erro no console do back-end
     console.error(error)
 
@@ -27,53 +27,43 @@ controller.create = async function(req, res) {
 
 controller.retrieveAll = async function(req, res) {
   try {
-    const include = includeRelations(req.query)
-
-    // Manda buscar os dados no servidor
+    // Incluindo os produtos relacionados ao fornecedor
     const result = await prisma.fornecedor.findMany({
-      orderBy: [ { razao_social: 'asc' } ],
-      include
-    })
+      orderBy: [{ razao_social: 'asc' }],
+      include: { produtos: true }  // Inclui a relação de produtos
+    });
 
-    // Retorna os dados obtidos ao cliente com o status
-    // HTTP 200: OK (implícito)
-    res.send(result)
+    // Retorna os dados obtidos ao cliente
+    res.send(result);
   }
-  catch(error) {
+  catch (error) {
     // Deu errado: exibe o erro no console do back-end
-    console.error(error)
+    console.error(error);
 
     // Envia o erro ao front-end, com status 500
-    // HTTP 500: Internal Server Error
-    res.status(500).send(error)
+    res.status(500).send(error);
   }
 }
 
 controller.retrieveOne = async function(req, res) {
   try {
-
-    const include = includeRelations(req.query)
-
-    // Manda buscar o documento no servidor usando
-    // como critério de busca um id informado no
-    // parâmetro da requisição
+    // Incluindo os produtos relacionados ao fornecedor
     const result = await prisma.fornecedor.findUnique({
       where: { id: req.params.id },
-      include
-    })
+      include: { produtos: true }  // Inclui a relação de produtos
+    });
 
     // Encontrou o documento ~> retorna HTTP 200: OK (implícito)
-    if(result) res.send(result)
+    if (result) res.send(result)
     // Não encontrou o documento ~> retorna HTTP 404: Not Found
-    else res.status(404).end()
+    else res.status(404).end();
   }
-  catch(error) {
+  catch (error) {
     // Deu errado: exibe o erro no console do back-end
-    console.error(error)
+    console.error(error);
 
     // Envia o erro ao front-end, com status 500
-    // HTTP 500: Internal Server Error
-    res.status(500).send(error)
+    res.status(500).send(error);
   }
 }
 
@@ -85,20 +75,19 @@ controller.update = async function(req, res) {
     const result = await prisma.fornecedor.update({
       where: { id: req.params.id },
       data: req.body
-    })
+    });
 
     // Encontrou e atualizou ~> retorna HTTP 204: No Content
-    if(result) res.status(204).end()
+    if (result) res.status(204).end();
     // Não encontrou (e não atualizou) ~> retorna HTTP 404: Not Found
-    else res.status(404).end()
+    else res.status(404).end();
   }
-  catch(error) {
+  catch (error) {
     // Deu errado: exibe o erro no console do back-end
-    console.error(error)
+    console.error(error);
 
     // Envia o erro ao front-end, com status 500
-    // HTTP 500: Internal Server Error
-    res.status(500).send(error)
+    res.status(500).send(error);
   }
 }
 
@@ -108,26 +97,25 @@ controller.delete = async function(req, res) {
     // como parâmetro e efetua a exclusão caso encontrado
     await prisma.fornecedor.delete({
       where: { id: req.params.id }
-    })
+    });
 
     // Encontrou e excluiu ~> HTTP 204: No Content
-    res.status(204).end()
-
+    res.status(204).end();
   }
-  catch(error) {
-    if(error?.code === 'P2025') {   // Código erro de exclusão no Prisma
+  catch (error) {
+    if (error?.code === 'P2025') {   // Código erro de exclusão no Prisma
       // Não encontrou e não excluiu ~> HTTP 404: Not Found
-      res.status(404).end()
+      res.status(404).end();
     }
     else {
       // Outros tipos de erro
-      console.error(error)
+      console.error(error);
 
       // Envia o erro ao front-end, com status 500
       // HTTP 500: Internal Server Error
-      res.status(500).send(error)
+      res.status(500).send(error);
     }
   }
 }
 
-export default controller
+export default controller;
---
+++ back-end/src/routes/vendas.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import controller from '../controllers/vendas.js'
+
+const router = Router()
+
+router.post('/', controller.create)
+router.get('/', controller.retrieveAll)
+router.get('/:id', controller.retrieveOne)
+router.put('/id', controller.update)
+router.delete('/id', controller.delete)
+
+// Rotas para os itens da venda
+//router.post('/:id/itens', controller.createItem)
+//router.get('/:id/itens', controller.retrieveAllItems)
+//router.get('/:id/itens', controller.retrieveOneItem)
+//router.put('/:id/itens', controller.retrieveOneItem)
+//router.
+
+
+export default router
