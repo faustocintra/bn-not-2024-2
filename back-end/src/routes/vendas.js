d8d47a5832aebfe9b00905445d01486143c16644
ellendias01 <ellendias25@hotmail.com>
2024-09-30 20:06:19 -0300

    (30/09) Término do back-end de exemplo

--- back-end/src/routes/vendas.js
+++ back-end/src/routes/vendas.js
@@ -9,12 +9,11 @@ router.get('/:id', controller.retrieveOne)
 router.put('/:id', controller.update)
 router.delete('/:id', controller.delete)
 
-// Rotas para os intens da venda
-
-router.post('/:id/itens' , controller.createItem)
+// Rotas para os itens da venda
+router.post('/:id/itens', controller.createItem)
 router.get('/:id/itens', controller.retrieveAllItems)
 router.get('/:id/itens/:itemId', controller.retrieveOneItem)
 router.put('/:id/itens/:itemId', controller.updateItem)
-router.delete('/:id/itens/:itemID' , controller.deleteItem)
+router.delete('/:id/itens/:itemId', controller.deleteItem)
 
 export default router
