491f1726a6ca7ae6a5e9d45d225c4003221e1054
Sheila Alves <135647046+Sheila724@users.noreply.github.com>
2024-09-30 20:11:11 -0300

    (30/09) Termino do back-end de exemplo

--- back-end/src/routes/vendas.js
+++ back-end/src/routes/vendas.js
@@ -1,19 +1,19 @@
-import { Router } from 'express';
-import controller from '../controllers/vendas.js';
+import { Router } from 'express'
+import controller from '../controllers/vendas.js'
 
-const router = Router();
+const router = Router()
 
-router.post('/', controller.create);
-router.get('/', controller.retrieveAll);
-router.get('/:id', controller.retrieveOne);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.delete);
+router.post('/', controller.create)
+router.get('/', controller.retrieveAll)
+router.get('/:id', controller.retrieveOne)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
 // Rotas para os itens da venda
-router.post('/:id/itens', controller.createItem);
-router.post('/:id/itens', controller.retrieveAllItems);
-// router.get('/:id/itemId', controller.retrieveOneItem);
-// router.put('/:id/itemId', controller.updateItem);
-// router.delete('/:id/itemId', controller.deleteItem);
+router.post('/:id/itens', controller.createItem)
+router.get('/:id/itens', controller.retrieveAllItems)
+router.get('/:id/itens/:itemId', controller.retrieveOneItem)
+router.put('/:id/itens/:itemId', controller.updateItem)
+router.delete('/:id/itens/:itemId', controller.deleteItem)
 
-export default router;
+export default router
